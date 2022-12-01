e7c52748f9b3fece31beb3e401f5698494bdc7ac
natauana <natauananoleto@gmail.com>
2022-12-01 14:42:04 -0300

    criação da classe pessoa

---
+++ src/Pessoa.java
@@ -0,0 +1,30 @@
+public class Pessoa {
+
+    private int id_pessoa, cpf;
+    private String nome;
+
+
+    public int getId_pessoa() {
+        return this.id_pessoa;
+    }
+
+    public void setId_pessoa(int id_pessoa) {
+        this.id_pessoa = id_pessoa;
+    }
+
+    public int getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+
+    
+}
