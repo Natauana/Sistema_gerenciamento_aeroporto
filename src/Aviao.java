ec1b42fbe0dd327392f72786a4b65b3ff7161be4
claryi <clara.almeida@estudante.ifto.edu.br>
2022-12-07 14:43:55 -0300

    criacao das classes Voo Aeroporto e Aviao

---
+++ src/Aviao.java
@@ -0,0 +1,57 @@
+public class Aviao {
+    private double carga;
+    private int id;
+    private String cod_aviao;
+    private String cia_aerea;
+    private String poutronas;
+    private int total_passageiros;
+
+    public double getCarga() {
+        return this.carga;
+    }
+
+    public void setCarga(double carga) {
+        this.carga = carga;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCod_aviao() {
+        return this.cod_aviao;
+    }
+
+    public void setCod_aviao(String cod_aviao) {
+        this.cod_aviao = cod_aviao;
+    }
+
+    public String getCia_aerea() {
+        return this.cia_aerea;
+    }
+
+    public void setCia_aerea(String cia_aerea) {
+        this.cia_aerea = cia_aerea;
+    }
+
+    public String getPoutronas() {
+        return this.poutronas;
+    }
+
+    public void setPoutronas(String poutronas) {
+        this.poutronas = poutronas;
+    }
+
+    public int getTotal_passageiros() {
+        return this.total_passageiros;
+    }
+
+    public void setTotal_passageiros(int total_passageiros) {
+        this.total_passageiros = total_passageiros;
+    }
+    
+}
---
+++ src/Voo.java
@@ -0,0 +1,22 @@
+public class Voo {
+    private double DiaVoo;
+    private  double horarioVoo;
+
+
+    public double getDiaVoo() {
+        return this.DiaVoo;
+    }
+
+    public void setDiaVoo(double DiaVoo) {
+        this.DiaVoo = DiaVoo;
+    }
+
+    public double getHorarioVoo() {
+        return this.horarioVoo;
+    }
+
+    public void setHorarioVoo(double horarioVoo) {
+        this.horarioVoo = horarioVoo;
+    }
+    
+}
