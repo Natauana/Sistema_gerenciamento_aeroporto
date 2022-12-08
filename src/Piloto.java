74d278f2cf833b171934a77a39515825e9d36475
unknown <Aluno@CLN-041435.ifto.local>
2022-12-08 16:47:03 -0300

    Criacao da classe Piloto

---
+++ src/Piloto.java
@@ -0,0 +1,22 @@
+public class Piloto extends Funcionario  {
+
+    private String cod_piloto;
+
+
+    public Piloto() {
+    }
+
+    public Piloto(String cod_piloto) {
+        this.cod_piloto = cod_piloto;
+    }
+
+    public String getCod_piloto() {
+        return this.cod_piloto;
+    }
+
+    public void setCod_piloto(String cod_piloto) {
+        this.cod_piloto = cod_piloto;
+    }
+ 
+}
+
